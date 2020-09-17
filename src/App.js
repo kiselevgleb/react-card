c2e9224d7221abef9dd02b2c34fd5a47540ce611
Пользователь <kiselevgleb@gmail.com>
2020-09-17 12:15:05 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Card from './Card';
 
 function App() {
+  const todos = [{ img: 'https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/composition/cards/assets/card2.png', title: 'Special title treatment', text: 'With supporting text below as a natural lead-in to additional content.', btntext: 'Go somewhere' },
+  { img: '', title: 'Card title', text: 'With supporting text below as a natural lead-in', btntext: 'Go somewhere!' }];
+  const listCards = todos.map((card) =>
+    <div className="card" style={{ width: '18rem', marginBottom: '5rem'}}>
+      <img src={card.img} className="card-img-top" alt=""></img>
+      <div className="card-body">
+        <h5 className="card-title">{card.title}</h5>
+        <p className="card-text">{card.text}</p>
+        <a href="#" className="btn btn-primary">{card.btntext}</a>
+      </div>
+    </div>);
+    // < Card > { card }</Card >);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      < Card>{listCards}</Card >  
     </div>
   );
 }
