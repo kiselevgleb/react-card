674d9a0cafb1d2b09354ce395c36f9eaf15ca2b5
Пользователь <kiselevgleb@gmail.com>
2020-09-17 12:23:06 +0300

    clean app

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const todos = [{ img: 'https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/composition/cards/assets/card2.png', title: 'Special title treatment', text: 'With supporting text below as a natural lead-in to additional content.', btntext: 'Go somewhere' },
   { img: '', title: 'Card title', text: 'With supporting text below as a natural lead-in', btntext: 'Go somewhere!' }];
   const listCards = todos.map((card) =>
-    <div className="card" style={{ width: '18rem', marginBottom: '5rem'}}>
+    <div className="card" style={{ width: '18rem', marginBottom: '5rem' }}>
       <img src={card.img} className="card-img-top" alt=""></img>
       <div className="card-body">
         <h5 className="card-title">{card.title}</h5>
@@ -14,11 +14,10 @@ function App() {
         <a href="#" className="btn btn-primary">{card.btntext}</a>
       </div>
     </div>);
-    // < Card > { card }</Card >);
 
   return (
     <div className="App">
-      < Card>{listCards}</Card >  
+      <Card>{listCards}</Card >
     </div>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,21 +1,7 @@
 import React from 'react';
 
-// function Card(props) {
-  const Card = props =>
+const Card = props =>
     <>
       {props.children}
-      {/* <img src={props.img} className="card-img-top" alt=""></img>
-      <div className="card-body">
-        <h5 className="card-title">{props.title}</h5>
-        <p className="card-text">{props.text}</p>
-        <a href="#" className="btn btn-primary">{props.btntext}</a>
-      </div> */}
     </>
-  // return (
-  //   <div className="card" style={{ width: '18rem' }}>
-  //     {props.children}
-  //   </div>
-  // );
-// }
-
 export default Card;
