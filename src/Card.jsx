4f397c778577821752e16fda9c4473e51d4777ad
Пользователь <kiselevgleb@gmail.com>
2020-09-28 15:10:18 +0300

    commit

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Card = props =>
-    <>
-      {props.children}
-    </>
+  <>
+    {props.todo.map((card) =>
+      <div className="card" style={{ width: '18rem', marginBottom: '5rem' }}>
+        <img src={card.img} className="card-img-top" alt=""></img>
+        <div className="card-body">
+          <h5 className="card-title">{card.title}</h5>
+          <p className="card-text">{card.text}</p>
+          <a href="#" className="btn btn-primary">{card.btntext}</a>
+        </div>
+      </div>)}
+  </>
 export default Card;
