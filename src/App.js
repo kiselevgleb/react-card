61a6eb6ef876c0f1fa9f0949ad5fd394e8c2d20e
Пользователь <kiselevgleb@gmail.com>
2020-10-07 22:17:07 +0300

    edited Card component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Card from './Card';
 
 function App() {
-  const todos = [{ img: 'https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/composition/cards/assets/card2.png', title: 'Special title treatment', text: 'With supporting text below as a natural lead-in to additional content.', btntext: 'Go somewhere' },
-  { img: '', title: 'Card title', text: 'With supporting text below as a natural lead-in', btntext: 'Go somewhere!' }];
+  
   return (
     <div className="App">
-      <Card todo={todos}></Card >
+      <Card title="Card title" text="With supporting text below as a natural lead-in to additional content." btntext="Go somewhere!" />
+      <Card title="Special title treatment" text="With supporting text below as a natural lead-in" btntext="Go somewhere!">
+        <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/composition/cards/assets/card2.png" />
+      </Card>
     </div>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Card = props =>
   <>
-    {props.todo.map((card) =>
-      <div className="card" style={{ width: '18rem', marginBottom: '5rem' }}>
-        <img src={card.img} className="card-img-top" alt=""></img>
-        <div className="card-body">
-          <h5 className="card-title">{card.title}</h5>
-          <p className="card-text">{card.text}</p>
-          <a href="#" className="btn btn-primary">{card.btntext}</a>
-        </div>
-      </div>)}
+    { <div className="card" style={{ width: '18rem', marginBottom: '5rem' }}>
+      {props.children}
+      <div className="card-body">
+        <h5 className="card-title">{props.title}</h5>
+        <p className="card-text">{props.text}</p>
+        <a href="#" className="btn btn-primary">{props.btntext}</a>
+      </div>
+    </div>}
   </>
+
 export default Card;
